59c50b43ce68513bf6f0b0edc1cd8751b7406175
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2023-05-16 20:09:18 +0400

    Update views

--- CharlistStarfinder/Charlist/urls.py
+++ CharlistStarfinder/Charlist/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import CharacterListlView
 
 urlpatterns = [
-    path('characters/', CharacterListlView.as_view(), name='character_list'),
+    path('', CharacterListlView.as_view(), name='character_list'),
 ]

18d11e3d7e906b16b4aeb915431135aa7b33be95
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2023-05-18 22:09:51 +0400

    Summation of characteristics

--- CharlistStarfinder/Charlist/models.py
+++ CharlistStarfinder/Charlist/models.py
@@ -63,7 +63,7 @@ class Character(AbstractCharacteristics):
     def __str__(self):
         return f"{self.name}"
 
-
+    # Калькулятор модификатора
     def calculate_modifiers(self):
         fields = [field for field in self._meta.get_fields() if not field.name.endswith('_modifier')]
         modifier = {0:-5, 1: -5, 2:-4, 3:-4, 4:-3, 5:-3, 6:-2, 7:-2, 8:-1, 9:-1, 10:0, 11:0, 12:1, 13:1, 14:2, 15:2, 16:3, 17:3, 18:4, 19:4, 20:5, 21:5, 22:6, 23:6, 24:7, 25:7, 26:8}
@@ -72,13 +72,24 @@ class Character(AbstractCharacteristics):
                 field_value = getattr(self, field.name)
                 setattr(self, f'{field.name}_modifier', modifier[field_value])
         
+    # Cуммирование характеристик
+    def calculate_characteristics(self):
+        fields = [field for field in self._meta.get_fields() if not field.name.endswith('_modifier')]
+        for field in fields:
+            if isinstance(field, models.IntegerField) and field.name != 'id' and field.name != 'hit_points':
+                field_value = getattr(self, field.name)
+                race_field_value = getattr(self.rase, field.name)
+                setattr(self, f'{field.name}', field_value + race_field_value)
 
+
+    # Рассчет пунктов здоровья
     @property
     def calculate_health(self):
         hit_points = self.rase.hit_points + self.character_class.hit_points
         return hit_points
 
     def save(self, *args, **kwargs):
+        self.calculate_characteristics()
         self.calculate_modifiers()
         self.hit_points = self.calculate_health
         super().save(*args, **kwargs)

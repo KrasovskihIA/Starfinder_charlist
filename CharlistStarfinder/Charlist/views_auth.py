c3d0fe700f0a1426e50a5f9a59491948814ed893
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2023-06-14 21:34:08 +0400

    LoginView

--- CharlistStarfinder/Charlist/views_auth.py
+++ CharlistStarfinder/Charlist/views_auth.py
@@ -1,2 +1,31 @@
 from django.contrib.auth.views import LoginView
-from .models import Profile
+from django.contrib.auth import authenticate, login, logout
+from .forms_auth import LoginForm, SignupForm
+from django.urls import reverse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import login_required
+
+
+# Представление для входа на аккаунт
+class Login(LoginView):
+    template_name = ''
+    form_class = LoginForm
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect(reverse('character_list'), request)
+        print(form.cleaned_data)
+        return render(request, self.template_name, context={'form': form})
+
+
+@login_required
+def logout_views(request):
+    logout(request)
+    return redirect(reverse('character_list'))

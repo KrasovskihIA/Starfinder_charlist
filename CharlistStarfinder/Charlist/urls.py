486cddcfecf7e4d1b7a21cc001bc7e29270b23fe
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2023-06-28 18:29:56 +0400

    CharacterEditView

--- CharlistStarfinder/Charlist/urls.py
+++ CharlistStarfinder/Charlist/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CharacterListlView, CharacterDetailView, CharacterCreateView
+from .views import CharacterListlView, CharacterDetailView, CharacterCreateView, CharacterEditView
 
 
 urlpatterns = [
     path('', CharacterListlView.as_view(), name='character_list'),
     path(f'character/<str:name>/', CharacterDetailView.as_view(), name='character_detail'),
     path('create/', CharacterCreateView.as_view(), name ='character_create'),
+    path('character/<str:name>/edit', CharacterEditView.as_view(), name='character_edit'),
 ]
--- CharlistStarfinder/Charlist/views.py
+++ CharlistStarfinder/Charlist/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.exceptions import PermissionDenied
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
-from .models import Character, CharacterClass, Race
+from .models import Character
+from audioop import reverse
 from .forms import CharacterForm
 from django.urls import reverse_lazy
-from django.utils.text import slugify
+
 
 # Отображение персонажей
 class CharacterListlView(ListView):
@@ -36,9 +38,9 @@ class CharacterCreateView(CreateView):
         form = self.form_class(request.POST, request.FILES)
         context = {}
         if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
+            character = form.save(commit=False)
+            character.author = request.user
+            character.save()
             context['character_was_created'] = True  
             context['form'] = self.form_class         
         else:
@@ -49,11 +51,23 @@ class CharacterCreateView(CreateView):
     def get_success_url(self):
         return reverse_lazy('character_list')
 
-
+# Редактирование персонажей 
 class CharacterEditView(UpdateView):
     model = Character
-    template_name = ''
+    slug_field = 'name'
+    slug_url_kwarg = 'name'
+    template_name = 'Charlist/character_edit.html'
     form_class = CharacterForm
+
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != request.user:
+            raise PermissionDenied('Вы не автор этого персонажа')
+        else:
+            return super(CharacterEditView, self).dispatch(request, *args, **kwargs)
+
+    def get_success_url(self):
+        return reverse_lazy('character_list')
 
 
 

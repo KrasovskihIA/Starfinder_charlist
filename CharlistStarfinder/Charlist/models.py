4e58f209b2cd0771006b7464908a028a8ad4af41
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2023-07-04 20:42:10 +0400

    Delete calculate_characteristics

--- CharlistStarfinder/Charlist/models.py
+++ CharlistStarfinder/Charlist/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 
 
 
+
 # Генерация пути для аватара персонажа
 def character_avatar_path(instance, filename):
         return f'{instance.name}/avatar/{filename}'
@@ -67,7 +68,7 @@ class CharacterTheme(AbstractCharacteristics):
 
 # Персонаж
 class Character(AbstractCharacteristics):
-    author = models.OneToOneField(User, on_delete=models.CASCADE, related_name='characters_as_author')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='characters_as_author')
     name = models.CharField('Имя', max_length=50)
     avatar = models.ImageField(upload_to=character_avatar_path, blank=True)
     rase = models.ForeignKey(Race, on_delete=models.CASCADE, related_name='characters_as_race')
@@ -90,23 +91,15 @@ class Character(AbstractCharacteristics):
     # Калькулятор модификатора
     def calculate_modifiers(self):
         fields = [field for field in self._meta.get_fields() if not field.name.endswith('_modifier')]
-        modifier = {0:-5, 1: -5, 2:-4, 3:-4, 4:-3, 5:-3, 6:-2, 7:-2, 8:-1, 9:-1, 10:0, 11:0, 12:1, 13:1, 14:2, 15:2, 16:3, 17:3, 18:4, 19:4, 20:5, 21:5, 22:6, 23:6, 24:7, 25:7, 26:8}
-        for field in fields:
-            if isinstance(field, models.IntegerField) and field.name != 'id' and field.name != 'hit_points':
-                field_value = getattr(self, field.name)
-                setattr(self, f'{field.name}_modifier', modifier[field_value])
-        
-    # Cуммирование характеристик
-    def calculate_characteristics(self):
-        fields = [field for field in self._meta.get_fields() if not field.name.endswith('_modifier')]
+        modifier = {0:-5, 1: -5, 2:-4, 3:-4, 4:-3, 5:-3, 6:-2, 7:-2, 8:-1, 9:-1, 10:0, 11:0, 12:+1, 13:1, 14:2, 15:2, 16:3, 17:3, 18:4, 19:4, 20:5, 21:5, 22:6, 23:6, 24:7, 25:7, 26:8}
         for field in fields:
             if isinstance(field, models.IntegerField) and field.name != 'id' and field.name != 'hit_points':
                 field_value = getattr(self, field.name)
                 race_field_value = getattr(self.rase, field.name)
                 theme_field_value = getattr(self.theme, field.name)
-                setattr(self, f'{field.name}', field_value + race_field_value + theme_field_value)
-
-
+                setattr(self, f'{field.name}_modifier', modifier[field_value + race_field_value + theme_field_value])
+        
+ 
     # Рассчет пунктов здоровья
     @property
     def calculate_health(self):
@@ -114,7 +107,6 @@ class Character(AbstractCharacteristics):
         return hit_points
 
     def save(self, *args, **kwargs):
-        self.calculate_characteristics()
         self.calculate_modifiers()
         self.hit_points = self.calculate_health
         super().save(*args, **kwargs)
